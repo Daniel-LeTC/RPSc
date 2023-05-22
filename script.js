41bfd2e669de8c13a91882126676fcbbab2367df
Daniel <117688408+Daniel-LeTC@users.noreply.github.com>
2023-05-22 22:11:42 +0700

    add function to get random computer choice for this game

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let a = 1;
+const selections = ["rock" , "paper" , "scissors"];
+let computerSelection;
+
+function getComputerChoice (computerSelection) { //This is the function that will generate random computer selection for this game
+
+return computerSelection = selections[Math.floor(Math.random()*selections.length)];
+
+};
+
+
+
+console.log (getComputerChoice (computerSelection));
